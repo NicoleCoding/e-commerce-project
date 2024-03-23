71a69b876bf44d8e8f0c9085a907e0ea39bd3365
NicoleCoding <nicole251097@gmail.com>
2024-03-23 11:56:56 +0200

    Initial commit of e-commerce project.

---
+++ js/basket.js
@@ -0,0 +1,103 @@
+let basketElem = document.getElementById("shopping-basket"); // Container for the items inside the basket.
+let checkoutMessageElem = document.getElementById("checkout-message"); // Container for checkout message.
+let backToProductsElem = document.getElementById("back-to-products"); // Container for the link to go back to product page.
+
+
+
+
+// Generates the items which are added in the basket.
+function generateBasketItems() {
+  if (basket.length !== 0) {  // If the length of the basket array is not 0, items inside the basket page are generated.
+    return (basketElem.innerHTML = basket.map((x) => { // New array generated containing x and the new array displayed inside the basket container.
+      let { id, quantity } = x; // Destructuring the object x.
+      let search = products.find((y) => y.id === id) || []; // Search an item with id in basket. Otherwise return an empty array if nothing found.
+      return `
+          <div class="basket-item">
+            <img width="100" src=${search.image} alt="" />
+            <div class="details">
+              <p>${search.name}</p>
+              <p class="cart-item-price">Price per item: ${search.price}:-</p>
+            </div>
+            <div class="buttons">
+              <i onclick="decrementProductsInBasket(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">${quantity}</div>
+              <i onclick="incrementProductsInBasket(${id})" class="bi bi-plus-lg"></i>
+              </div>
+          </div>`;}).join(""));
+  } else {  
+    basketElem.innerHTML = ``; // If the length of basket array is 0, the element for the basket remains empty.
+    backToProductsElem.innerHTML = `<a href="index.html">Back to products page</a>`; // Element which contains a link to go back to product page.
+  }
+
+}
+
+generateBasketItems();
+
+// Function to increment the items in basket
+function incrementProductsInBasket(id) {
+  let selectedItem = id; // Variable for item being selected.
+  let search = basket.find((x) => x.id === selectedItem.id); // Searches for item with specific id inside basket.
+
+  if (search === undefined) { 
+    basket.push({ // If no item with specific id found, item is added into basket.
+      id: selectedItem.id,
+      quantity: 1,
+    });
+  }
+  else {
+    search.quantity += 1; // If item with specific id exists, increase quantity by 1.
+  }
+
+  generateBasketItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+
+}
+// Function for the decrement button in the basket. Works the same way as the increment function; however it also checks if quantity is equal to 0 and it that case, nothing happens.
+function decrementProductsInBasket(id) {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined)
+    return;
+  else if (search.quantity === 0)
+    return; // Nothing happens, meaning the quantity cannot be decreased once value of quantity is 0.
+  else {
+    search.quantity -= 1;
+  }
+
+  basket = basket.filter((x) => x.quantity !== 0);
+  generateBasketItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+
+}
+
+// Function which clears the basket.
+function clearBasket() {
+  basket = []; // Empty array to reset basket.
+  generateBasketItems(); // Call the function for generating basket items to generate page without basket items.
+  localStorage.setItem("data", JSON.stringify(basket)); // Data saved inside local storage.
+  checkoutMessageElem.innerHTML = "";
+
+}
+
+// Function which calculates the total sum.
+function totalAmount() {
+  if (basket.length !== 0) { 
+    let sum = basket.map((x) => { 
+      let { quantity, id } = x; // Destructure the x to get the quantity and id of the object which is x.
+      let search = products.find((y) => y.id === id) || []; // Search an item with id in basket. Otherwise return an empty array if nothing found.
+
+      return quantity * search.price; // Returns the total sum of each product.
+    }).reduce((x, y) => x + y, 0); // All objects inside the array are summarized to get the total sum of all products.
+
+    
+    checkoutMessageElem.innerHTML = `<h2>The total sum is ${sum}</h2>`; // Write out the total sum to the user inside the specified container.
+  }
+  else
+    return; // Nothing happens if basket length is 0.
+
+
+}
+
+
+  
---
+++ js/products.js
@@ -0,0 +1,116 @@
+const products =
+  [
+    {
+        "id": "KFNKGGF",
+        "name": "Apple tree",
+        "description": "Apples are the by far the easiest fruits to grow. You can buy a tree or several here, ready to be planted and with just a little care, you can enjoy delicious apples!",
+        "price": "150",
+        "image": "images/appletree.jpg"
+    },
+    {   "id": "FDSKKGL",
+        "name": "Cherry tree",
+        "description": "Cherry trees require more thought than apple trees. They require drained and fertile soil in order to grow properly in addition to 8 hours sunlight a day.",
+        "price": "300",
+        "image": "images/cherrytree.jpg"
+    },
+    {   "id": "FGGDSFD",
+        "name": "Lemon tree",
+        "description": "Lemon trees are more sensitive to cold compared to other fruit trees. Therefore it is important to plant lemon trees closer to the house or even indoors in a pot.",
+        "price": "200",
+        "image": "images/lemontree.jpg"
+    },
+    {   "id": "OKPGPG",
+        "name": "Orange tree",
+        "description": "Orange trees are similar to lemon trees, sensitive to cold. Therefore take care to plant it in a pot or appropriate spot. Here at us you can get a potted orange tree.",
+        "price": "220",
+        "image": "images/orangetree.jpg"
+     }
+  ];  
+
+
+let productsElem = document.getElementById("products"); // Container for the products
+let basket = JSON.parse(localStorage.getItem("data")) || []; // Items which were selected, are stored inside this basket array. Basket either has the added products which were saved inside local storage or is empty.
+
+// Displays products on the webpage.
+function renderProducts() {
+  return (productsElem.innerHTML = products.map((x) => { // Creates a new array for every object in products and the new array is displayed inside the productsElem.
+    let { id, name, price, description, image } = x; // Destructuring the object.
+    let search = basket.find((x) => x.id === id) || []; // Search an item with id in basket. Otherwise return an empty array if nothing found.
+    return `<div id=product-id-${id} class="item">
+              <img width="250" height="250" src=${image} alt="">
+              <div class="details">
+                <h3>${name}</h3>
+                <p>${description}</p>
+                <div class="price-quantity">
+                  <h2>${price}:- </h2>
+                  <div class="buttons">
+                    <i onclick="decrementProduct(${id})" class="bi bi-dash-lg">Remove item from basket</i> 
+                   <div id=${id} class="quantity">
+                     ${search.quantity === undefined ? 0 : search.quantity}
+                   </div>
+                   <i onclick="incrementProduct(${id})" class="bi bi-plus-lg">Add item to basket</i>
+                 </div>
+                </div>
+              </div>
+            </div>`;}).join("")); // Template literal is returned and contains the data for each product
+
+
+
+
+}
+renderProducts(); 
+
+// Increase the quantity of the product.
+function incrementProduct(id) {
+  let selectedItem = id; // Variable for the item which is being selected.
+  let search = basket.find((x) => x.id === selectedItem.id); // Tries to find an item with a specific id in the basket.
+  if (search === undefined) { 
+    basket.push({             // If the search determines that an item does not exist in the basket yet, it will push a new object inside the basket.
+      id: selectedItem.id,
+      quantity: 1,
+    });
+  } else { 
+    search.quantity += 1;  // If the item already exists, the quantity is increased by 1.
+  }
+
+
+  updateBasketIcon(selectedItem.id); // Runs this function and updates the selected item.
+  localStorage.setItem("data", JSON.stringify(basket)); // The data inside the basket is stored in local storage.
+
+}
+
+// Decrease the quantity of the product, works the same way as the increment function but in reverse. Also if the quantity is 0, nothing happens.
+function decrementProduct(id) {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+  if (search === undefined)
+    return;
+  else if (search.quantity === 0) // Nothing happens if quantity is equal to 0.
+    return;
+  else {
+    search.quantity -= 1;
+  }
+  updateBasketIcon(selectedItem.id); 
+  basket = basket.filter((x) => x.quantity !== 0); // Filters the objects inside the basket which do not have a quantity of 0 and these are passed inside an array.
+  localStorage.setItem("data", JSON.stringify(basket)); 
+
+
+}
+
+// Updates the basket icon on the productpage.
+function updateBasketIcon(id) {
+  let search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.quantity; // Element with id "id" displays the quantity of all products inside the basket.
+  calculationBasketIcon(); // Runs the calculation of all added products.
+}
+
+// Calculates quantity of the items in the basket which are displayed at the basket icon.
+function calculationBasketIcon() {
+  let basketAmount = document.getElementById("basketAmount"); // The basket icon is declared and assigned to the element with id "basketAmount".
+  basketAmount.innerHTML = basket.map((x) => x.quantity).reduce((x, y) => x + y, 0); // Create an array with only the quantities of the products inside the basket, then all the quantities are summed up with the reduce function and this is displayed inside the basketIcon element.
+
+}
+
+calculationBasketIcon();
+
+
